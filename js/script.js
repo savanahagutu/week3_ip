78b23969d7843bc0f7e8320be528fe6914c551c5
savanahatieno <savanaht01@gmail.com>
2021-03-03 17:59:48 +0300

    Created a message display

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ $(document) . ready(function(){
   });
 
 });
+
+
+/*Submit section*/
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
+    if ($("input#name").val() && $("input#email").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
+
+});
