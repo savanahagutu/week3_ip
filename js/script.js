5e38ceac91fc4264f238f8f0fb61dbc4afb5a424
savanahatieno <savanaht01@gmail.com>
2021-03-03 12:49:26 +0300

    initial chnages

--- js/script.js
+++ js/script.js
@@ -1,38 +1,17 @@
-$(document).ready(function(){
-    $("#button_1").click(function(){
-      $("#button_1").slideDown('1500').hide('1000');
-      $("#design").show('1500');
-    });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#button_1").slideDown('1500');
-    });
+$(document) . ready(function(){
+  $("#design").click(function(){
+    $("p1").toggle("slow");
+    $(".image1").toggle("slow");
   });
-  
 
-
-
-
-
-$(document).ready(function(){
-    $("#button_2").click(function(){
-      $("#button_2").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#button_2").slideDown('1500');
-    });
+  $("#development").click(function(){
+    $("p2").toggle("slow");
+    $(".image2").toggle("slow");
   });
-  
-  
-  $(document).ready(function(){
-    $("#button_3").click(function(){
-      $("#button_3").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#button_3").slideDown('1500');
-    });
+
+  $("#product").click(function(){
+    $("p3").toggle("slow");
+    $(".image3").toggle("slow");
   });
+
+});
